ce7d102457739bb911f2b58ad4edff97aeb18d50
Agustino Whickey <wakembus98@gmail.com>
2022-10-15 22:01:54 +0700

    Dashboard grafik sort by

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -22,10 +22,10 @@ class Admin extends CI_Controller {
 				ORDER BY sold DESC");
 		$result = $query->result();
 
-		$query2 = $this->db->query("SELECT SUM(unit_qty * unit_price) AS outcome FROM stock_item");
+		$query2 = $this->db->query("SELECT SUM(stock * unit_price) AS outcome FROM item");
 		$result2 = $query2->result();
 
-		$query3 = $this->db->query("SELECT sale_detail.id AS saleid, sale.created, product_item.name, (SELECT SUM(sale_detail.qty)) AS sold, (SELECT SUM(sale.total_price)) AS total_penjualan
+		$query3 = $this->db->query("SELECT sale_detail.id AS saleid, sale.created, sale.date, product_item.name, (SELECT SUM(sale_detail.qty)) AS sold, (SELECT SUM(sale.total_price)) AS total_penjualan
 				FROM sale_detail
 					INNER JOIN sale ON sale_detail.sale_id = sale.id
 					INNER JOIN product_item ON sale_detail.item_id = product_item.id
@@ -34,8 +34,11 @@ class Admin extends CI_Controller {
 				ORDER BY sold DESC");
 		$result3 = $query3->result();
 
-		$query4 = $this->db->query("SELECT SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(created), '%m') = MONTH(CURRENT_DATE()) THEN 0 ELSE unit_qty * unit_price END) AS outcome FROM stock_item");
+		$query4 = $this->db->query("SELECT SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(created), '%m') = MONTH(CURRENT_DATE()) THEN 0 ELSE stock * unit_price END) AS outcome FROM item");
 		$result4 = $query4->result();
+
+		$query5 = $this->db->query("SELECT stock * unit_price AS total_pengeluaran, created FROM item WHERE (DATE_FORMAT(FROM_UNIXTIME(created), '%m')) = MONTH(CURRENT_DATE()) GROUP BY id");
+		$result5 = $query5->result();
 
 		$total_penjualan = 0;
 		$total_item_terjual = 0;
@@ -51,7 +54,8 @@ class Admin extends CI_Controller {
 			$total_item_terjual_bulan_ini += (int)$value->sold;
 		}
 		
-		$data['chart'] = $result;
+		$data['chart'] = $result3;
+		$data['chart2'] = $result5;
 		$data['user'] 	= $this->login_m->ceklogin($this->session->userdata('email'));
 		$data['total_penjualan'] = indo_currency($total_penjualan);
 		$data['total_item_terjual'] = $total_item_terjual;
@@ -81,7 +85,7 @@ class Admin extends CI_Controller {
 				ORDER BY sold DESC");
 		$result = $query->result();
 
-		$query2 = $this->db->query("SELECT SUM(unit_qty * unit_price) AS outcome FROM stock_item");
+		$query2 = $this->db->query("SELECT SUM(stock * unit_price) AS outcome FROM item");
 		$result2 = $query2->result();
 
 		$query3 = $this->db->query("SELECT sale_detail.id AS saleid, sale.created, sale.date, product_item.name, (SELECT SUM(sale_detail.qty)) AS sold, (SELECT SUM(sale.total_price)) AS total_penjualan
@@ -93,8 +97,11 @@ class Admin extends CI_Controller {
 				ORDER BY sold DESC");
 		$result3 = $query3->result();
 
-		$query4 = $this->db->query("SELECT SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(created), '%m') = WEEK(CURRENT_DATE()) THEN 0 ELSE unit_qty * unit_price END) AS outcome FROM stock_item");
+		$query4 = $this->db->query("SELECT SUM(CASE WHEN YEARWEEK(DATE_FORMAT(FROM_UNIXTIME(created), '%Y-%m-%d'), 1) >= YEARWEEK(CURDATE(), 1) THEN 0 ELSE stock * unit_price END) AS outcome FROM item");
 		$result4 = $query4->result();
+
+		$query5 = $this->db->query("SELECT stock * unit_price AS total_pengeluaran, created FROM item WHERE (YEARWEEK(DATE_FORMAT(FROM_UNIXTIME(created), '%Y-%m-%d'), 1) >= YEARWEEK(CURDATE(), 1)) GROUP BY id");
+		$result5 = $query5->result();
 
 		$total_penjualan = 0;
 		$total_item_terjual = 0;
@@ -111,6 +118,7 @@ class Admin extends CI_Controller {
 		}
 		
 		$data['chart'] = $result3;
+		$data['chart2'] = $result5;
 		$data['user'] 	= $this->login_m->ceklogin($this->session->userdata('email'));
 		$data['total_penjualan'] = indo_currency($total_penjualan);
 		$data['total_item_terjual'] = $total_item_terjual;
@@ -140,20 +148,28 @@ class Admin extends CI_Controller {
 				ORDER BY sold DESC");
 		$result = $query->result();
 
-		$query2 = $this->db->query("SELECT SUM(unit_qty * unit_price) AS outcome FROM stock_item");
+		$query2 = $this->db->query("SELECT SUM(stock * unit_price) AS outcome FROM item");
 		$result2 = $query2->result();
 
-		$query3 = $this->db->query("SELECT sale_detail.id AS saleid, sale.created, sale.date, product_item.name, (SELECT SUM(sale_detail.qty)) AS sold, (SELECT SUM(sale.total_price)) AS total_penjualan
+		$query3 = $this->db->query("SELECT MONTHNAME(DATE_FORMAT(FROM_UNIXTIME(sale.created), '%Y-%m-%d')) AS tgl, sale_detail.id AS saleid, sale.created, sale.date, product_item.name, (SELECT SUM(sale_detail.qty)) AS sold, (SELECT SUM(sale.total_price)) AS total_penjualan
 				FROM sale_detail
 					INNER JOIN sale ON sale_detail.sale_id = sale.id
 					INNER JOIN product_item ON sale_detail.item_id = product_item.id
-					WHERE (DATE_FORMAT(FROM_UNIXTIME(sale.created), '%Y') >= YEAR(CURDATE()))
-				GROUP BY sale_detail.item_id
-				ORDER BY sold DESC");
+					WHERE (DATE_FORMAT(FROM_UNIXTIME(sale.created), '%Y') = YEAR(CURDATE()))
+				GROUP BY DATE_FORMAT(FROM_UNIXTIME(sale.created), '%Y-%m-%d')
+				ORDER BY DATE_FORMAT(FROM_UNIXTIME(sale.created), '%Y-%m-%d') ASC");
 		$result3 = $query3->result();
-
-		$query4 = $this->db->query("SELECT SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(created), '%m') = WEEK(CURRENT_DATE()) THEN 0 ELSE unit_qty * unit_price END) AS outcome FROM stock_item");
+		
+		$query4 = $this->db->query("SELECT SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(created), '%Y') >= YEAR(CURDATE()) THEN 0 ELSE stock * unit_price END) AS outcome FROM item");
 		$result4 = $query4->result();
+
+		$query5 = $this->db->query("SELECT MONTHNAME(DATE_FORMAT(FROM_UNIXTIME(updated), '%Y-%m-%d')) AS tgl, stock * unit_price AS total_pengeluaran, updated FROM item WHERE (DATE_FORMAT(FROM_UNIXTIME(created), '%Y') = YEAR(CURDATE())) GROUP BY DATE_FORMAT(FROM_UNIXTIME(created), '%Y-%m-%d')");
+		$result5 = $query5->result();
+
+		// echo "<pre>";
+		// print_r($result5);
+		// echo "</pre>";
+		// exit;
 
 		$total_penjualan = 0;
 		$total_item_terjual = 0;
@@ -170,6 +186,8 @@ class Admin extends CI_Controller {
 		}
 		
 		$data['chart'] = $result3;
+		$data['chart_year'] = true;
+		$data['chart2'] = $result5;
 		$data['user'] 	= $this->login_m->ceklogin($this->session->userdata('email'));
 		$data['total_penjualan'] = indo_currency($total_penjualan);
 		$data['total_item_terjual'] = $total_item_terjual;
